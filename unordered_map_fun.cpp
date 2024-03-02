254365f334b8519d32790a3bd45173ef97bdae38
Tilak Gupta <gupta.tilak@hotmail.com>
2024-03-02 10:43:42 -0800

    Playing w/ unordered_map

---
+++ unordered_map_fun.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <utility>
+#include <initializer_list>
+
+struct Point {
+  int x;
+  int y;
+  int z;
+};
+int main() {
+    Point p{1, 2, 3};
+    std::cout << "p: " << p.x << ", " << p.y << ", " << p.z << std::endl;
+    std::initializer_list<Point> points{(1, 2, 3)};
+    int a = (1, 2, 3);
+    std::cout << a << std::endl;
+    int i=1;
+    for(auto p : points) {
+        std::cout << "Point #" << i++ << ": " << p.x << ", " << p.y << ", " << p.z << std::endl;
+    }
+    std::pair<std::string, int> p1("hello", 1);
+    std::unordered_map<std::string, int> um{
+        {"hello", 1},
+        {"world", 2},
+    };
+}
